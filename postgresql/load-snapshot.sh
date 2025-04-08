6a90f89bb32310d2306839e50195550c17ae4952
George Gatheca <george.gatheca@galaxy.com>
2025-04-08 12:15:37 +0100

    Db snapshot restore

--- postgresql/load-snapshot.sh
+++ postgresql/load-snapshot.sh
@@ -1,35 +1,27 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Define paths
+DUMP_DIR="/tmp"
+DUMP_TAR="${DUMP_DIR}/external_node.tar.gz"
+DUMP_LIST="${DUMP_DIR}/external_node/pg_restore.list"
+EXTRACTED_DUMP="${DUMP_DIR}/external_node/dump"
+
 if [ -n "${PG_SNAPSHOT}" ]; then
-  export PGPASSWORD="${POSTGRES_PASSWORD}"
-  export PGUSER="${POSTGRES_USER}"
+  echo "[*] Downloading snapshot file"
+  aria2c -c -x6 -s6 --auto-file-renaming=false --conditional-get=true --allow-overwrite=true -d /tmp -o "$DUMP_TAR" "${PG_SNAPSHOT}"
+  
+  # Extract the dump
+  echo "[*] Extracting pgdump..."
+  tar -xvzf "$DUMP_TAR" -C "$DUMP_DIR"
 
-  echo "Downloading snapshot file"
-  aria2c -c -x6 -s6 --auto-file-renaming=false --conditional-get=true --allow-overwrite=true -d /tmp \
-    -o snapshot.pgdump "${PG_SNAPSHOT}"
+  # Perform the restoration
+  echo "[*] Restoring database..."
+  pg_restore -x -O -j2 -L "$DUMP_LIST" -d "$POSTGRES_DB" -U "$POSTGRES_USER" "$EXTRACTED_DUMP"
 
-  if [ "${CALL_TRACES:-false}" = "false" ]; then
-    echo "Restoring pgdump without call_traces table"
-    pg_restore -l /tmp/snapshot.pgdump  | grep -iv 'call_traces' >/tmp/snapshot.list
-    __list="-L /tmp/snapshot.list"
-  else
-    echo "Restoring pgdump with call_traces table"
-    __list=""
-  fi
+  echo "[+] Database restoration complete."
 
-  __parallel=$(($(nproc)/2))
-  if [ "${__parallel}" -lt 2 ]; then
-    __parallel=2
-  fi
-
-# shellcheck disable=SC2086
-  pg_restore -O -v -j ${__parallel} -d "${POSTGRES_DB}" ${__list} /tmp/snapshot.pgdump
-
-  echo "Removing snapshot file"
-  rm -f /tmp/snapshot.pgdump
-
-  if [ "${CALL_TRACES:-false}" = "false" ]; then # create an empty table so migrations work
-    psql -d "${POSTGRES_DB}" -c "CREATE TABLE call_traces (tx_hash bytea not null primary key references transactions on delete cascade, call_trace bytea not null);"
-  fi
+  # Cleanup dump files
+  echo "[*] Cleaning up dump files to save space..."
+  rm -f "$DUMP_DIR/*"
 fi
